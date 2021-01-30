fbb768afa160b9fb51c2221f6024605ef349d0a7
pioneerplat <pioneerplat@gmail.com>
2021-01-31 06:24:31 +0900

    16 custom progress indicator 만들기

--- flutter_instagram_two/lib/widgets/post.dart
+++ flutter_instagram_two/lib/widgets/post.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram_two/widgets/my_progress_indicator.dart';
 
 class Post extends StatelessWidget {
   final int index;
@@ -16,18 +17,9 @@ class Post extends StatelessWidget {
     //flutter에 있는 이미지 위젯은 cache를하지않음(메모리에 저장을 하지 않음)
     //이미지를 핸드폰에 저장해 놓고 다른화면에 갔다가 다시 왔을때 저장한 이미지를 사용
     return CachedNetworkImage(
-      imageUrl: 'https://picsum.photos/id/$index/5000/5000',
+      imageUrl: 'https://picsum.photos/id/$index/200/200',
       placeholder: (BuildContext context, String url) {
-        return Container(
-            width: size.width,
-            height: size.width,
-            child: Center(
-                child: SizedBox(
-                    height: 60,
-                    width: 60,
-                    child: CircularProgressIndicator(
-                      backgroundColor: Colors.black87,
-                    ))));
+        return MyProgressIndicator(containerSize: size.width);
       },
       //imageUrl에서 다운로드 받은 이미지를 imageProvider를 통해서 가져온다
       imageBuilder: (BuildContext context, ImageProvider imageProvider) {
