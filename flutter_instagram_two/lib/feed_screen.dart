631c49b94b985c9bba96c7cb5986dff17e42aa1a
pioneerplat <pioneerplat@gmail.com>
2021-01-30 09:07:51 +0900

    12 appbar(actionbar)
    Column, Row, ListView

--- flutter_instagram_two/lib/feed_screen.dart
+++ flutter_instagram_two/lib/feed_screen.dart
@@ -1,23 +1,47 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
+      appBar: CupertinoNavigationBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.camera_alt,
+            color: Colors.black87,
+          ),
+          onPressed: null,
+        ),
+        middle: Text(
           'instagram',
           style: TextStyle(fontFamily: 'VeganStyle', color: Colors.black87),
         ),
+        trailing: Row(
+          //좌우의 사이즈를 최소한의 공간만 차지하게
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              onPressed: null,
+              icon: ImageIcon(
+                AssetImage('assets/images/actionbar_camera.png'),
+                color: Colors.black87,
+              ),
+            )
+          ],
+        ),
       ),
-      body: ListView.builder(itemBuilder: feedListBuilder,itemCount: 30,),
+      body: ListView.builder(
+        itemBuilder: feedListBuilder,
+        itemCount: 30,
+      ),
     );
   }
 }
 
-Widget feedListBuilder(BuildContext context, int index){
+Widget feedListBuilder(BuildContext context, int index) {
   return Container(
-    color: Colors.accents[index%Colors.accents.length],
+    color: Colors.accents[index % Colors.accents.length],
     height: 100,
   );
 }
