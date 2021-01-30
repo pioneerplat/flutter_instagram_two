7de73c0dbf8b63bb6e0a02c96373af9d3d5f2bb5
pioneerplat <pioneerplat@gmail.com>
2021-01-30 09:37:35 +0900

    14 온라인 image 가져와서 보여주기

--- flutter_instagram_two/lib/widgets/post.dart
+++ flutter_instagram_two/lib/widgets/post.dart
@@ -1,18 +1,31 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-
-
 
 class Post extends StatelessWidget {
   final int index;
-  const Post(this.index,{
+
+  const Post(
+    this.index, {
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.accents[index % Colors.accents.length],
-      height: 100,
+    //flutter에 있는 이미지 위젯은 cache를하지않음(메모리에 저장을 하지 않음)
+    //이미지를 핸드폰에 저장해 놓고 다른화면에 갔다가 다시 왔을때 저장한 이미지를 사용
+    return CachedNetworkImage(
+      imageUrl: 'https://picsum.photos/id/$index/200/300',
+      //imageUrl에서 다운로드 받은 이미지를 imageProvider를 통해서 가져온다
+      imageBuilder: (BuildContext context, ImageProvider imageProvider) {
+        return AspectRatio(
+          aspectRatio: 1,
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+            ),
+          ),
+        );
+      },
     );
   }
 }
