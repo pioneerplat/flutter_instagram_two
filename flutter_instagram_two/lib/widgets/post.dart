ca391e3c4910ee8f04143229230bada1a3161b5f
pioneerplat <pioneerplat@gmail.com>
2021-01-31 07:19:17 +0900

    18 extract 위젯

--- flutter_instagram_two/lib/widgets/post.dart
+++ flutter_instagram_two/lib/widgets/post.dart
@@ -1,6 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram_two/constants/common_size.dart';
 import 'package:flutter_instagram_two/widgets/my_progress_indicator.dart';
+import 'package:flutter_instagram_two/widgets/rounded_Avatar.dart';
 
 class Post extends StatelessWidget {
   final int index;
@@ -16,6 +18,34 @@ class Post extends StatelessWidget {
     if (size == null) size = MediaQuery.of(context).size;
     //flutter에 있는 이미지 위젯은 cache를하지않음(메모리에 저장을 하지 않음)
     //이미지를 핸드폰에 저장해 놓고 다른화면에 갔다가 다시 왔을때 저장한 이미지를 사용
+    return Column(
+      children: [
+        _postHeader(),
+        _postImage(),
+      ],
+    );
+  }
+
+  Widget _postHeader() {
+    return Row(
+      children: <Widget> [
+        Padding(
+          padding: const EdgeInsets.all(common_xxs_gap),
+          child: RoundedAvatar(),
+        ),
+        Expanded(child: Text("username")),
+        IconButton(
+          onPressed: null,
+          icon: Icon(
+            Icons.more_horiz,
+            color: Colors.black87,
+          ),
+        )
+      ],
+    );
+  }
+
+  CachedNetworkImage _postImage() {
     return CachedNetworkImage(
       imageUrl: 'https://picsum.photos/id/$index/200/200',
       placeholder: (BuildContext context, String url) {
@@ -35,3 +65,5 @@ class Post extends StatelessWidget {
     );
   }
 }
+
+
