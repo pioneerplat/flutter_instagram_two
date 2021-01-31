9285daa380dd9f4ef12f8be2d85de23ef7833f5f
pioneerplat <pioneerplat@gmail.com>
2021-02-01 07:00:00 +0900

    25 custom selection button

--- flutter_instagram_two/lib/screens/profile_screen.dart
+++ flutter_instagram_two/lib/screens/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram_two/constants/common_size.dart';
+import 'package:flutter_instagram_two/widgets/profile_body.dart';
 
 class ProfileScreen extends StatelessWidget {
   @override
@@ -10,61 +11,13 @@ class ProfileScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             _appbar(),
-            CustomScrollView(
-              slivers: [
-                SliverList(
-                    delegate: SliverChildListDelegate([
-                  _username(),
-                  _userBio(),
-                  _editProfileBtn(),
-                ]))
-              ],
-            )
+            ProfileBody(),
           ],
         ),
       ),
     );
   }
 
-  Padding _editProfileBtn() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: common_gap),
-      child: SizedBox(
-        height: 24,
-        child: OutlineButton(
-            onPressed: null,
-            borderSide: BorderSide(
-              color: Colors.black38,
-            ),
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
-            child: Text(
-              'Edit Profile',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            )),
-      ),
-    );
-  }
-
-  Widget _username() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: common_gap),
-      child: Text(
-        'username',
-        style: TextStyle(fontWeight: FontWeight.bold),
-      ),
-    );
-  }
-
-  Widget _userBio() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: common_gap),
-      child: Text(
-        'this is what I believe!!',
-        style: TextStyle(fontWeight: FontWeight.w400),
-      ),
-    );
-  }
 
   Row _appbar() {
     return Row(
