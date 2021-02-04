34362afc5e1d27f336959a11ad7207afc3bf0631
pioneerplat <pioneerplat@gmail.com>
2021-02-05 05:59:07 +0900

    40 SignUpForm 안에 ListView 사용

--- flutter_instagram_two/lib/screens/auth_screen.dart
+++ flutter_instagram_two/lib/screens/auth_screen.dart
@@ -15,46 +15,48 @@ class _AuthScreenState extends State<AuthScreen>
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Stack(
-        children: [
-          FadeStack(
-            selectedForm: selectedForm,
-          ),
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            FadeStack(
+              selectedForm: selectedForm,
+            ),
 
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            height: 40,
-            child: Container(
-              color: Colors.white,
-              child: FlatButton(
-                shape: Border(top: BorderSide(color: Colors.grey)),
-                onPressed: () {
-                  setState(() {
-                    selectedForm == 0 ? selectedForm = 1 : selectedForm = 0;
-                  });
-                },
-                child: RichText(
-                  text: TextSpan(
-                      text: (selectedForm == 0)
-                          ? 'Already have an account?'
-                          : "Don't have an account?",
-                      style: TextStyle(color: Colors.grey[500]),
-                      children: [
-                        TextSpan(text: '  '),
-                        TextSpan(
-                            text: (selectedForm == 0) ? 'Sign In' : 'Sign Up',
-                            style: TextStyle(
-                                color: Colors.blue,
-                                fontWeight: FontWeight.bold)),
-                      ]),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              height: 40,
+              child: Container(
+                color: Colors.white,
+                child: FlatButton(
+                  shape: Border(top: BorderSide(color: Colors.grey)),
+                  onPressed: () {
+                    setState(() {
+                      selectedForm == 0 ? selectedForm = 1 : selectedForm = 0;
+                    });
+                  },
+                  child: RichText(
+                    text: TextSpan(
+                        text: (selectedForm == 0)
+                            ? 'Already have an account?'
+                            : "Don't have an account?",
+                        style: TextStyle(color: Colors.grey[500]),
+                        children: [
+                          TextSpan(text: '  '),
+                          TextSpan(
+                              text: (selectedForm == 0) ? 'Sign In' : 'Sign Up',
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold)),
+                        ]),
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
