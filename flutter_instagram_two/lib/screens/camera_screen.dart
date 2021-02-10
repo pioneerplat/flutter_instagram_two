a4ea9a2121830e656684a91fabc3affcaf86a87c
pioneerplat <pioneerplat@gmail.com>
2021-02-10 22:43:11 +0900

    53 BottomNavigationBar without Icon

--- flutter_instagram_two/lib/screens/camera_screen.dart
+++ flutter_instagram_two/lib/screens/camera_screen.dart
@@ -6,10 +6,36 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return Scaffold(
+      body: PageView(
+        children: [
+          Container(color: Colors.green,),
+          Container(color: Colors.blue,),
+          Container(color: Colors.red,),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          iconSize: 0,
+          selectedItemColor: Colors.black87,
+          unselectedItemColor: Colors.black54,
+          selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+          items: [
+        BottomNavigationBarItem(icon: Icon(Icons.add), label: 'GALLERY'),
+        BottomNavigationBarItem(icon: Icon(Icons.add), label: 'PHOTO'),
+        BottomNavigationBarItem(icon: Icon(Icons.add), label: 'VIDEO'),
+      ],
+        currentIndex: _currentIndex,
+        onTap: _onItemTabbed,
+      ),
     );
+  }
+  
+  void _onItemTabbed(index) {
+      setState(() {
+        _currentIndex = index;
+      });
   }
 }
