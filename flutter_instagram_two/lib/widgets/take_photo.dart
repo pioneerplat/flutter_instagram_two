31bd0195830deb61350f18bbc653853fd758b6a9
pioneerplat <pioneerplat@gmail.com>
2021-02-14 09:33:36 +0900

    62 crop camera preview

--- flutter_instagram_two/lib/widgets/take_photo.dart
+++ flutter_instagram_two/lib/widgets/take_photo.dart
@@ -50,8 +50,24 @@ class _TakePhotoState extends State<TakePhoto> {
       builder: (context, snapshot) {
         //_controller.initialize() 는 void 이기때문에 hasData로 확인할 수 없다 
         if(snapshot.connectionState == ConnectionState.done) {
-          return CameraPreview(
-              _controller
+          return ClipRect(
+            //Overflow된 부분을 잘라준다
+            child: OverflowBox(
+              //center에 맞게 정렬시키고 박스 바깥으로 나갈 수 있게 해준다
+              alignment: Alignment.center,
+              child: FittedBox(
+                //가로길이에 맞게끔 비율을 조절한다 
+                fit: BoxFit.fitWidth,
+                child: Container(
+                  width: size.width,
+                  //가로로 긴 비율을 세로로 긴 비율로 바꿔준다
+                  height: size.width / _controller.value.aspectRatio,
+                  child: CameraPreview(
+                      _controller
+                  ),
+                ),
+              ),
+            ),
           );
         } else {
           return _progress;
