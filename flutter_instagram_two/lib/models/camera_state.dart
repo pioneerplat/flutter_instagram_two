2ba35796b0d80bcfc5ad8d723ac9d8567f83f76d
pioneerplat <pioneerplat@gmail.com>
2021-02-14 22:21:22 +0900

    66 build camera state model(change notifier model)

--- flutter_instagram_two/lib/models/camera_state.dart
+++ flutter_instagram_two/lib/models/camera_state.dart
@@ -6,4 +6,38 @@ class CameraState extends ChangeNotifier{
   CameraDescription _cameraDescription;
   bool _readyTakePhoto = false;
 
+  void getReadyToTakePhoto() async {
+    List<CameraDescription> cameras =await availableCameras();
+    
+    if(cameras != null && cameras.isNotEmpty){
+      setCameraDescription(cameras[0]);
+    }
+
+    bool init = false;
+    while(!init){
+      init = await initialize();
+    }
+
+    _readyTakePhoto = true;
+    //데이터 변화에 따른 디스플레이 업데이트를 해줘라
+    notifyListeners();
+  }
+  
+  void setCameraDescription(CameraDescription cameraDescription){
+    _cameraDescription = cameraDescription;
+    _controller = CameraController(_cameraDescription, ResolutionPreset.medium);
+  }
+
+  Future<bool> initialize() async {
+    try{
+      await _controller.initialize();
+      return true;
+    } catch(e) {
+      return false;
+    }
+  }
+  //밖에서 접근할 수 있도록
+  CameraController get controller => _controller;
+  CameraDescription get description => _cameraDescription;
+  bool get isReadyToTakePhoto => _readyTakePhoto;
 }
