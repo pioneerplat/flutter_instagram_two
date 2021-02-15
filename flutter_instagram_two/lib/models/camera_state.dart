176fd64758b3637d76ca7aed2c67bf4f413199c0
pioneerplat <pioneerplat@gmail.com>
2021-02-15 09:25:20 +0900

    69 prevent memory leak by disposing the controller

--- flutter_instagram_two/lib/models/camera_state.dart
+++ flutter_instagram_two/lib/models/camera_state.dart
@@ -1,20 +1,29 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-class CameraState extends ChangeNotifier{
+class CameraState extends ChangeNotifier {
   CameraController _controller;
   CameraDescription _cameraDescription;
   bool _readyTakePhoto = false;
 
+  void dispose() {
+    if (_controller != null) _controller.dispose();
+    _controller = null;
+    _cameraDescription = null;
+    _readyTakePhoto = false;
+    //해당 데이터가 변경이 되었다는 것을 Consumer에 알려준다
+    notifyListeners();
+  }
+
   void getReadyToTakePhoto() async {
-    List<CameraDescription> cameras =await availableCameras();
-    
-    if(cameras != null && cameras.isNotEmpty){
+    List<CameraDescription> cameras = await availableCameras();
+
+    if (cameras != null && cameras.isNotEmpty) {
       setCameraDescription(cameras[0]);
     }
 
     bool init = false;
-    while(!init){
+    while (!init) {
       init = await initialize();
     }
 
@@ -22,22 +31,25 @@ class CameraState extends ChangeNotifier{
     //데이터 변화에 따른 디스플레이 업데이트를 해줘라
     notifyListeners();
   }
-  
-  void setCameraDescription(CameraDescription cameraDescription){
+
+  void setCameraDescription(CameraDescription cameraDescription) {
     _cameraDescription = cameraDescription;
     _controller = CameraController(_cameraDescription, ResolutionPreset.medium);
   }
 
   Future<bool> initialize() async {
-    try{
+    try {
       await _controller.initialize();
       return true;
-    } catch(e) {
+    } catch (e) {
       return false;
     }
   }
+
   //밖에서 접근할 수 있도록
   CameraController get controller => _controller;
+
   CameraDescription get description => _cameraDescription;
+
   bool get isReadyToTakePhoto => _readyTakePhoto;
 }
