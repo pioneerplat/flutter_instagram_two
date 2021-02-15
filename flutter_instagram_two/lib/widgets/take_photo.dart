428d9b63b9a0ca8c4c7791824622cea475209aff
pioneerplat <pioneerplat@gmail.com>
2021-02-16 05:14:31 +0900

    71 share post screen

--- flutter_instagram_two/lib/widgets/take_photo.dart
+++ flutter_instagram_two/lib/widgets/take_photo.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram_two/constants/screen_size.dart';
 import 'package:flutter_instagram_two/models/camera_state.dart';
+import 'package:flutter_instagram_two/screens/share_post_screen.dart';
 import 'package:flutter_instagram_two/widgets/my_progress_indicator.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
@@ -30,20 +33,19 @@ class _TakePhotoState extends State<TakePhoto> {
                 width: size.width,
                 height: size.width,
                 color: Colors.black,
-                child: (cameraState.isReadyToTakePhoto) ? _getPreview(
-                    cameraState) : _progress
-            ),
+                child: (cameraState.isReadyToTakePhoto)
+                    ? _getPreview(cameraState)
+                    : _progress),
             Expanded(
                 child: OutlineButton(
-                  onPressed: () {
-                    if (cameraState.isReadyToTakePhoto) {
-                      _attemptTakePhoto()
-                    }
-                  },
-                  shape: CircleBorder(),
-                  borderSide: BorderSide(color: Colors.black12, width: 20),
-                )
-            )
+              onPressed: () {
+                if (cameraState.isReadyToTakePhoto) {
+                  _attemptTakePhoto(cameraState, context);
+                }
+              },
+              shape: CircleBorder(),
+              borderSide: BorderSide(color: Colors.black12, width: 20),
+            ))
           ],
         );
       },
@@ -63,32 +65,30 @@ class _TakePhotoState extends State<TakePhoto> {
             width: size.width,
             //가로로 긴 비율을 세로로 긴 비율로 바꿔준다
             height: size.width / cameraState.controller.value.aspectRatio,
-            child: CameraPreview(
-                cameraState.controller
-            ),
+            child: CameraPreview(cameraState.controller),
           ),
         ),
       ),
     );
   }
 
-  void _attemptTakePhoto(CameraState cameraState) async {
+  void _attemptTakePhoto(CameraState cameraState, BuildContext context) async {
     //이미지명으로 사용하기 위해
-    final String timeInMilli = DateTime
-        .now()
-        .millisecondsSinceEpoch
-        .toString();
+    final String timeInMilli = DateTime.now().millisecondsSinceEpoch.toString();
     try {
       //.path를 하면 String으로 변환된다
       //await getTemporaryDirectory()).path 저장되는 폴더 위치
       //join은 String 을 합쳐주는 연산자 (path.dart)
-      final path = join(
-          (await getTemporaryDirectory()).path, '$timeInMilli.png');
+      final path =
+          join((await getTemporaryDirectory()).path, '$timeInMilli.png');
 
       //사진 찍기 (path 에다가 사진을 저장한다)
       await cameraState.controller.takePicture(path);
-    } catch (e) {
 
-    }
+      File imageFile = File(path);
+      //builder: (_) 해준 이유는 sharePostScreen을 할때 context가 필요없기 때문에 언더바 처리를 해줌
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (_) => SharePostScreen(imageFile)));
+    } catch (e) {}
   }
 }
